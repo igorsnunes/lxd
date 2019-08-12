b6e4a8c15efa3bde8ef9d5849b9ca3efeacd41a8
igorsnunes <igor.snunes@gmail.com>
2019-08-12 03:08:17 +0000

    Limpeza de saida do lab2

--- inf572/lab2/testa-ambiente.sh
+++ inf572/lab2/testa-ambiente.sh
@@ -6,8 +6,8 @@ IP6="2001:db8:2018:A::10 2001:db8:2018:A::100 2001:db8:2018:B::100 2001:db8:2018
 
 testa_conectividade() {
 	for i in $1; do
-		lxc exec A2 -- /bin/ping$2 -c 3 $i > /dev/null && \
-			lxc exec A2 -- /usr/sbin/traceroute -n $i > /dev/null
+		lxc exec A2 -- /bin/ping$2 -c 3 $i &> /dev/null && \
+			lxc exec A2 -- /usr/sbin/traceroute -n $i &> /dev/null
 		if [ $? -eq 0 ]; then
 			echo "Conectividade com $i : ok "
 		else
@@ -18,6 +18,7 @@ testa_conectividade() {
 
 echo "verificando conectividade IPv4 a partir de A2"
 testa_conectividade "$IP4"
+echo "verificando conectividade IPv6 a partir de A2"
 testa_conectividade "$IP6" 6
 
 echo "verificando DNS a partir de A2"
