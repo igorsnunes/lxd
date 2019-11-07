bea18d3c51221d907945e12ceaeb49f10034f382
igorsnunes <igor.snunes@gmail.com>
2019-11-07 03:13:12 +0000

    realiza correcoes na topologia

--- inf574/final/gera-ambiente.sh
+++ inf574/final/gera-ambiente.sh
@@ -36,6 +36,7 @@ echo "Criando redes "
 lxc network create DMZ ipv4.address=192.168.20.254/24 ipv4.nat=false ipv4.dhcp=false
 lxc network create RSERVERS ipv4.address=192.168.30.254/24 ipv4.nat=false ipv4.dhcp=false
 lxc network create RWEB ipv4.address=192.168.40.254/24 ipv4.nat=false ipv4.dhcp=false
+lxc network create R1R2 ipv4.address=192.168.50.254/24 ipv4.nat=false ipv4.dhcp=false
 
 ## DNS AUTH
 vm_gen "DNSAUTH" "DMZ" "eth0" "192.168.20.4" "192.168.20.1"
@@ -72,21 +73,44 @@ lxc copy debian9padrao R1
 
 echo "Ligando interfaces de R1"
 lxc network attach DMZ R1 eth1
-lxc network attach R2 R1 eth2 ## REVER! TEM QUE LIGAR EM F2
-#lxc network attach F1 R1 eth3 ## REVER! TEM QUE LIGAR EM F1
-
+[ $? -ne 0 ] && echo "erro ligando interfaces de DMZ-R1"
+lxc network attach R1R2 R1 eth2 ## REVER! TEM QUE LIGAR EM F2
+[ $? -ne 0 ] && echo "erro ligando interfaces de R2-R1"
 
 echo "Ligando interfaces de R2"
 lxc network attach RWEB R2 eth1
+[ $? -ne 0 ] && echo "erro ligando interfaces de RWEB-R2"
 lxc network attach RSERVERS R2 eth2
-lxc network attach R1 R2 eth3 ## rever, tem que ligar no firewall
+[ $? -ne 0 ] && echo "erro ligando interfaces de RSERVER-R2"
+lxc network attach R1R2 R2 eth3 ## rever, tem que ligar no firewall
+[ $? -ne 0 ] && echo "erro ligando interfaces de R1-R2"
 
 echo "Copiando configuracoes"
-interface_gen "eth1" "192.168.20.1" "ipdainternet" "eth2" "192.168.40.1" "ipdainternet" "eth3" "ipdainternet" "ipdainternet"# REVER
+
+echo "auto lo">/tmp/interfaces
+echo "iface lo inet loopback">>/tmp/interfaces
+echo "auto eth0">>/tmp/interfaces
+echo "iface eth0 inet dhcp">>/tmp/interfaces
+echo "auto eth1">>/tmp/interfaces
+echo "iface eth1 inet static">>/tmp/interfaces
+echo "	address 192.168.20.1/24">>/tmp/interfaces
+echo "auto eth2">>/tmp/interfaces
+echo "	iface eth2 inet static">>/tmp/interfaces
+echo "	address 192.168.50.1/24">>/tmp/interfaces
 lxc file push /tmp/interfaces R1/etc/network/interfaces
 
-interface_gen "eth1" "192.168.20.1" "ipdainternet" "eth2" "192.168.40.1" "ipdainternet" # REVER
-lxc file push /tmp/interfaces R1/etc/network/interfaces
+echo "auto lo">/tmp/interfaces
+echo "iface lo inet loopback">>/tmp/interfaces
+echo "auto eth1">>/tmp/interfaces
+echo "iface eth1 inet static">>/tmp/interfaces
+echo "	address 192.168.50.2/24">>/tmp/interfaces
+echo "auto eth2">>/tmp/interfaces
+echo "	iface eth2 inet static">>/tmp/interfaces
+echo "	address 192.168.30.1/24">>/tmp/interfaces
+echo "auto eth3">>/tmp/interfaces
+echo "	iface eth3 inet static">>/tmp/interfaces
+echo "	address 192.168.40.1/24">>/tmp/interfaces
+lxc file push /tmp/interfaces R2/etc/network/interfaces
 
 
 echo "Iniciando containers"
@@ -96,3 +120,8 @@ do
 done
 echo "Aguardando 10 segundos para garantir que R esta no ar"
 sleep 10
+for i in "DNSAUTH" "PROXY" "SSHS" "WWW1" "WWW2" "SLOG" "DNSREC" "R2" "R1"
+do
+	echo "Instalando ssh em $i"
+	lxc exec $i -- apt install -y openssh-server
+done
