43e54d8e8b512704147977c44720482e1b8f2dae
igorsnunes <igor.snunes@gmail.com>
2019-11-13 02:46:43 +0000

    corrige rotas, instala ssh e corrige caminhos

--- inf574/final/gera-ambiente.sh
+++ inf574/final/gera-ambiente.sh
@@ -34,7 +34,7 @@ function vm_gen() {
 
 echo "Criando redes "
 lxc network delete DMZ 
-lxc network create DMZ ipv4.address=192.168.20.254/24 ipv4.nat=true ipv4.dhcp=false
+lxc network create DMZ ipv4.address=192.168.20.254/24 ipv4.nat=false ipv4.dhcp=false
 lxc network delete RSERVERS
 lxc network create RSERVERS ipv4.address=192.168.40.254/24 ipv4.nat=true ipv4.dhcp=false
 lxc network delete RWEB
@@ -101,6 +101,7 @@ echo "net.ipv6.conf.all.forwarding=1" >> /tmp/sysctl.conf
 lxc file push /tmp/sysctl.conf R1/etc/sysctl.conf
 lxc file push /tmp/sysctl.conf R2/etc/sysctl.conf
 
+
 echo "auto lo">/tmp/interfaces
 echo "iface lo inet loopback">>/tmp/interfaces
 echo "auto eth0">>/tmp/interfaces
@@ -132,6 +133,7 @@ for i in "DNSAUTH" "PROXY" "SSHS" "WWW1" "WWW2" "SLOG" "DNSREC" "R2" "R1"
 do
 	lxc start $i
 done
+
 echo "Aguardando 10 segundos para garantir que R esta no ar"
 sleep 10
 
@@ -143,6 +145,15 @@ lxc exec R1 -- ip route add 192.168.40.0/24 via 192.168.50.2
 lxc exec R2 -- ip route add default via 192.168.50.1
 lxc exec R2 -- ip route add 192.168.20.0/24 via 192.168.50.1
 
+echo "Configurando nat de R1"
+lxc exec R1 -- /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+
+
+echo "Iniciando containers"
+for i in "DNSAUTH" "PROXY" "SSHS" "WWW1" "WWW2" "SLOG" "DNSREC" "R2" "R1"
+do
+	lxc start $i
+done
 
 echo "INSTALANDO openssh-server e fail2ban em SSHS"
 ./instala-ssh.sh SSHS
@@ -151,6 +162,16 @@ echo "INSTALANDO nginx em PROXY"
 ./gera-proxy.sh PROXY
 
 echo "INSTALANDO apache em WWW1 e WWW2"
-cd /home/inf500/574-SDN/lxd/inf574/final/www
+cd www
 ./instala-www.sh WWW1
 ./instala-www.sh WWW2
+cd -
+
+for j in "openssh-server"
+do
+	echo "Instalando  $j nos containers"
+	for i in "DNSAUTH" "PROXY" "SSHS" "WWW1" "WWW2" "SLOG" "DNSREC" "R2" "R1"
+	do
+		lxc exec $i -- /usr/bin/apt install -y $j 
+	done
+done
